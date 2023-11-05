a7713ee0281bff33f627b72808aefb013c6dd80e
Moudi07 <126020301+Moudi07@users.noreply.github.com>
2023-11-05 18:40:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ def input_converter(inp):
     arr = scaled_data.transform(arr)
     prediction = loaded_model.predict(arr)
 
-    return (f"The Fuel Consumption L/100km is {rounded_prediction}")
+    return (f"The Fuel Consumption L/100km is {rounded_prediction[0]}")
 
 
 
